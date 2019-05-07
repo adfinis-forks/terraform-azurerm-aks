39848b6b9d3c0e62a0f719a704a75d23cb7298e7
Michael Imfeld <michael.imfeld@adfinis-sygroup.ch>
2019-05-07 12:57:08 +0200

    Migrate to terraform 0.12

--- outputs.tf
+++ outputs.tf
@@ -1,35 +1,36 @@
 output "client_key" {
-  value = "${module.kubernetes.client_key}"
+  value = module.kubernetes.client_key
 }
 
 output "client_certificate" {
-  value = "${module.kubernetes.client_certificate}"
+  value = module.kubernetes.client_certificate
 }
 
 output "cluster_ca_certificate" {
-  value = "${module.kubernetes.cluster_ca_certificate}"
+  value = module.kubernetes.cluster_ca_certificate
 }
 
 output "kube_config_raw" {
-  value = "${module.kubernetes.raw_kube_config}"
+  value = module.kubernetes.raw_kube_config
 }
 
 output "host" {
-  value = "${module.kubernetes.host}"
+  value = module.kubernetes.host
 }
 
 output "username" {
-  value = "${module.kubernetes.username}"
+  value = module.kubernetes.username
 }
 
 output "password" {
-  value = "${module.kubernetes.password}"
+  value = module.kubernetes.password
 }
 
 output "node_resource_group" {
-  value = "${module.kubernetes.node_resource_group}"
+  value = module.kubernetes.node_resource_group
 }
 
 output "location" {
-  value = "${module.kubernetes.location}"
+  value = module.kubernetes.location
 }
+
